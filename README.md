b923579a8de3bb77241b0d10764e36a8db0ea389
Toby Gaskell <Toby96@sky.com>
2025-07-09 00:39:25 +0100

    adding in end to end intergration tests to test the api once it is running

--- README.md
+++ README.md
@@ -11,40 +11,28 @@ kubectl rollout restart deployment fpg-api -n testing
 kubectl rollout restart deployment fpg-api -n prod
 ```
 
+### To Run Pytests Locally
+
+`
+uv run pytest
+
+```
+
 ### Build & Run docker image locally
 
 ```
+
 docker build -t fpg .
 docker run -p 8000:8000 --env-file .env fpg
+
 ```
 
 ### To run locally so it will refresh with changes
 
 ```
+
 docker compose up --build
+
 ```
 
-| Endpoint                 | Season Added | Tested |
-| ------------------------ | ------------ | ------ |
-| current_round            | N/A          | N/A    |
-| deactivate_notifications | N/A          | N/A    |
-| get_available_choices    | True         | False  |
-| get_choices              | True         | False  |
-| get_fixtures             | True         | False  |
-| get_player_info          | True         | False  |
-| get_points               | True         | False  |
-| get_previous_choices     | True         | False  |
-| get_previous_points      | True         | False  |
-| get_rolling_standings    | True         | False  |
-| get_round_info           | True         | False  |
-| get_round_results        | True         | False  |
-| get_season_overview      | True         | False  |
-| get_standings            | True         | False  |
-| get_weekly_info          | True         | False  |
-| get_username             | N/A          | N/A    |
-| init_player              | N/A          | N/A    |
-| init_notifications       | N/A          | N/A    |
-| init_player_app          | N/A          | N/A    |
-| make_choice              | True         | False  |
-| update_choice            | True         | False  |
-| update_username          | N/A          | N/A    |
+```
---
+++ test_api/test_e2e.py
@@ -0,0 +1,273 @@
+# tests/integration/test_api_live.py
+
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+BASE_URL = os.getenv("BASE_URL")
+
+def test_rules():
+    """ """
+    r = requests.get(f"{BASE_URL}/rules")
+    assert r.status_code == 200
+
+def test_api():
+    """ """
+    r = requests.get(f"{BASE_URL}/api")
+    assert r.status_code == 200
+
+def test_index():
+    """ """
+    r = requests.get(f"{BASE_URL}/")
+    assert r.status_code == 200
+
+def test_get_current_round():
+    """ """
+    r = requests.get(f"{BASE_URL}/current_round?player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = {'Round ID', 'season'}
+
+    assert expected_keys.issubset(data.keys()), f"Response keys missing. Got keys: {data.keys()}"
+    assert isinstance(data['Round ID'], int), f"Round ID is not int: {data['Round ID']}"
+    assert isinstance(data['season'], int), f"season is not int: {data['season']}"
+
+
+def test_get_available_choices():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_available_choices?player_id=6&season=2025")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_length = 1
+    expected_keys = {'TEAM_NAME'}
+    
+    assert len(data) >= expected_length
+    assert isinstance(data, list), f"Available choices is not an array"
+    assert isinstance(data[0], dict)
+    assert expected_keys.issubset(data[0].keys())
+
+
+def test_get_choices_inc_method():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_choices?round_id=10&season=2024&inc_method=true")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = {'Choice', 'Method'}
+    expected_length = 1
+
+    assert len(data) >= expected_length
+    assert isinstance(data['6'], dict)
+    assert isinstance(data, dict)
+    assert expected_keys.issubset(data['6'].keys())
+    
+
+def test_get_choices_not_inc_method():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_choices?round_id=10&season=2024&inc_method=false")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_length = 1
+
+    assert len(data) >= expected_length
+    assert isinstance(data, dict)
+    assert isinstance(data['6'], str)
+
+
+def test_get_fixtures():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_fixtures?round_id=10&season=2024&player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+    expected_length = 10
+    expected_keys = { "AWAY_LOGO", "AWAY_TEAM", "DERBY", "FIXTURE_ID", "HOME_LOGO",
+                      "HOME_TEAM", "KICKOFF", "LOCATION", "ROUND", "SEASON" }
+
+    assert len(data) == expected_length
+    assert isinstance(data, list)
+    assert isinstance(data[0], dict)
+    assert expected_keys.issubset(data[0].keys())
+
+
+def test_get_player_info():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_player_info?season=2024&player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = { "draw_cnt", "lose_cnt", "round_cnt", "total_points", "win_cnt" }
+    expected_length = 1
+    
+    assert len(data) == expected_length
+    assert isinstance(data, list)
+    assert isinstance(data[0], dict)
+    assert expected_keys.issubset(data[0].keys())   
+
+
+def test_get_points():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_points?round_id=10&season=2024&player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = {"Basic", "Choice", "Derby", "Draw Means More", "Head 2 Head",
+                     "Result", "Subtotal", "Total", "User", "player_id"}
+    expected_length = 1
+
+    assert len(data) >= expected_length
+    assert isinstance(data, list)
+    assert isinstance(data[0], dict)
+    assert expected_keys.issubset(data[0].keys())
+
+
+def test_get_previous_choices():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_previous_choices?season=2024&player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = {"1st Pick", "2nd Pick", "Choice"}
+    expected_length = 20
+
+    assert len(data) == expected_length
+    assert isinstance(data, list)
+    assert isinstance(data[0], dict)
+    assert expected_keys.issubset(data[0].keys())
+
+
+def test_get_previous_points():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_previous_points?season=2024&player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = {"1st Pick", "2nd Pick", "Choice"}
+    expected_length = 20
+
+    assert len(data) == expected_length
+    assert isinstance(data, list)
+    assert isinstance(data[0], dict)
+    assert expected_keys.issubset(data[0].keys())
+
+
+def test_get_rolling_standings():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_rolling_standings?season=2024&player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = { "Goal Diff", "Position", "Score", "User", "player_id", "round" }
+    expected_length = 1
+
+    assert len(data) >= expected_length
+    assert isinstance(data, list)
+    assert isinstance(data[0], dict)
+    assert expected_keys.issubset(data[0].keys())
+
+
+def test_get_round_info():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_round_info?round_id=10&season=2024&player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = { "Cut Off", "DMM", "Double", "Round" }
+    expected_length = 4
+
+    assert len(data) == expected_length
+    assert isinstance(data, dict)
+    assert expected_keys.issubset(data.keys())
+
+
+def test_get_round_results():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_round_results?round_id=10&season=2024&player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = { "AWAY_GOALS", "FIXTURE_ID", "GAME_STATUS", "HOME_GOALS", "ROUND", "SCORE", "WINNER" }
+    expected_length = 10
+
+    assert len(data) == expected_length
+    assert isinstance(data, list)
+    assert isinstance(data[0], dict)
+    assert expected_keys.issubset(data[0].keys())
+
+
+def test_get_season_overview():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_season_overview?season=2024&player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = { "player_id", "POINTS", "RESULT", "round" }
+    expected_length = 1
+
+    assert len(data) >= expected_length
+    assert isinstance(data, list)
+    assert isinstance(data[0], dict)
+    assert expected_keys.issubset(data[0].keys())
+
+def test_get_standings():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_standings?season=2024&player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = { "Goal Diff", "Position", "Score", "User", "movement_indicator", "player_id" }
+    expected_length = 1
+
+    assert len(data) >= expected_length
+    assert isinstance(data, list)
+    assert isinstance(data[0], dict)
+    assert expected_keys.issubset(data[0].keys())
+
+def test_get_username():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_username?player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = { "Username" }
+    expected_length = 1
+
+    assert len(data) == expected_length
+    assert isinstance(data, dict)
+    assert expected_keys.issubset(data.keys())
+
+def test_get_weekly_info():
+    """ """
+    r = requests.get(f"{BASE_URL}/get_weekly_info?round_id=10&season=2024&player_id=6")
+    assert r.status_code == 200, f"Unexpected status code: {r.status_code}"
+
+    data = r.json()
+
+    expected_keys = { "away_goals", "away_team", "basic", "derby", "dmm", "Doubled", 
+                      "head_to_head", "home_goals", "home_team", "lonely_points", "pick", 
+                      "player_id", "round", "total", "subtotal", "running_total", 'docked_points'}
+    expected_length = 1
+
+    assert len(data) == expected_length
+    assert isinstance(data, list)
+    assert isinstance(data[0], dict)
+    assert expected_keys.issubset(data[0].keys())
